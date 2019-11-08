f8af658ce60e6c5bc2ed15dbc7879d4ab623dd68
Luis Augusto Quiroz Burga <4c1dhydr4@gmail.com>
2019-11-08 15:48:53 -0500

    Viernes de T3}

--- tools.py
+++ tools.py
@@ -4,3 +4,9 @@ def get_data(name):
 	data = pd.read_csv(name, sep='\t')
 	data = data.iloc[:,49:125]
 	return data
+
+def get_subsets(set):
+	return (set[0:1000, 0:5], set[1000::, 5:10])
+
+def get_Ysubset(set):
+    return (set[0:1000], set[1000::])
